bd6b37bf0c2a1e7e81d4e813dc9a6a50fc18ff3d
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-02-15 21:22:50 +0000

    Tutorial5 Commit. Adding and dynamically adding new articles.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,12 +8,15 @@ import { Article } from './article.model';
 })
 export class ArticleComponent {
 
-  article:Article;
+  @Input()
+  article!: Article;
 
 
   constructor() { 
-    this.article = new Article("Angular", "http://angular.io", 10)
-    
+    //this.article = new Article("Angular", "http://angular.io", 10)
+    new Article("Angular", "http://angular.io", 5),
+      new Article("Google", "http://google.com", 3),
+      new Article("TikTok", "http://tiktok.com", 1)
   }
 
   ngOnInit() { 
