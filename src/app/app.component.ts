2d44a360c9c7a0d079440402ff35185117c34a81
LeviG97 <98537056+LeviG97@users.noreply.github.com>
2023-02-15 21:56:22 +0000

    Tutorial6 Final Commit. Styled button. Added domain names to articles. Added fuctionality to sort articles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,8 @@ export class AppComponent {
     console.log(`Adding Article Title ${title.value} : and adding Article Link ${link.value}: `);
     return false;
   }
+
+  sortedArticles(): Article[]{
+    return this.articles.sort((a:Article, b: Article) => b.votes - a.votes);
+  }
 }
--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -18,4 +18,14 @@ export class Article{
     this.votes --;
     
   }
+
+  domain(): string {
+    try{
+      const link: string = this.link.split("//")[1];
+      return link.split("/")[0];
+    }
+    catch (err) {
+      return "";
+    }
+  }
 }
